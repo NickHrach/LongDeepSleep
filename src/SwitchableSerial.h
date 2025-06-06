a6adbf95113907c729e7d3acd0eefd8abc936489
unknown <NickHrach>
2025-06-06 18:07:37 +0200

    Added missing SwitchableSerial.h

---
+++ src/SwitchableSerial.h
@@ -0,0 +1,18 @@
+/* USAGE:
+#define _DEBUG_ 1    // SET TO 0 OUT TO REMOVE TRACES
+#include <SwitchabelSerial.h>
+*/
+
+#if _DEBUG_
+#define D_init(...) 	   do {if (Serial.baudRate()==0) Serial.begin(__VA_ARGS__);} while (0)
+#define D_print(...)       Serial.print(__VA_ARGS__)
+#define D_printf(...)      Serial.printf(__VA_ARGS__)
+#define D_write(...)       Serial.write(__VA_ARGS__)
+#define D_println(...)     Serial.println(__VA_ARGS__)
+#else
+#define D_SerialBegin(...)
+#define D_print(...)
+#define D_printf(...)
+#define D_write(...)
+#define D_println(...)
+#endif
