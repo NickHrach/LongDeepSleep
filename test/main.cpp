c68601511b51f5430fc97d6e7fcc7605593d4acd
unknown <215174764+NickHrach@users.noreply.github.com>
2025-06-23 11:19:25 +0200

    added docu

--- test/main.cpp
+++ test/main.cpp
@@ -7,10 +7,11 @@ NTPClient ntp;
 const char *ssid="Simulatet Wifi SSID";
 const char *password="Simulated Wifi password";
 
+
+// in a real restart the lds object would be created in a plain environment, hence we destroy and re-create it:
 #define SIMULATE_ESP_RESTART do {ESP.moc_restart(5);\
 			delete lds; \
 			lds= new LongDeepSleep(ssid, password, &ntp);} while(0)
-
 
 TEST(LongDeepSleepTest, LongRelativeSleep) {
 	lds= new LongDeepSleep(ssid, password, &ntp);
